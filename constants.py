6195dbbff039bcb0ab8493da53aee3ce2d4a1dab
lyle@melick.net <lyle@melick.net>
2017-09-29 10:00:28 -0400

    Change to using City ID to pull data, adding City ID to datafile.csv
    Will allow better database organization when the time comes.
    
    Signed-off-by: lyle@melick.net <lyle@melick.net>

--- constants.py
+++ constants.py
@@ -4,3 +4,4 @@ constants for studious-octo-fortnight project
 
 DEFAULT_API_KEY = "cf7844bf5d23112eb26c6feb5cb2d905"
 place_name = "Sunbury,OH"
+city_id = "5173412"
--- functions.py
+++ functions.py
@@ -12,49 +12,49 @@ def owm_status():
 
 def owm_observation(place_name):
     '''Return an Observation object in variable observation''' 
-    observation = owm.weather_at_place(place_name)
+    observation = owm.weather_at_id(city_id)
     return observation
 
 
 def owm_w(place_name):
     '''Return just the weather data from the object'''
-    w = owm.weather_at_place(place_name).get_weather()
+    w = owm.weather_at_id(city_id).get_weather()
     return w
 
 
 def owm_time(place_name):
     '''Return reading time'''
-    t = owm.weather_at_place(place_name).get_weather().get_reference_time(timeformat='iso')
+    t = owm.weather_at_id(city_id).get_weather().get_reference_time(timeformat='iso')
     time = t[:19]
     return time
 
 
 def owm_speed(place_name):
     '''Return current wind speed'''
-    speed = owm.weather_at_place(place_name).get_weather().get_wind()['speed']
+    speed = owm.weather_at_id(city_id).get_weather().get_wind()['speed']
     return speed
 
 
 def owm_direction(place_name):
     '''Return current wind direction in degrees'''
-    direction = owm.weather_at_place(place_name).get_weather().get_wind()['deg']
+    direction = owm.weather_at_id(city_id).get_weather().get_wind()['deg']
     return direction
 
 
 def owm_pressure(place_name):
     '''Return current barometric pressure'''
-    pressure = owm.weather_at_place(place_name).get_weather().get_pressure()['press']
+    pressure = owm.weather_at_id(city_id).get_weather().get_pressure()['press']
     return pressure
 
 
 def owm_temperature(place_name):
     '''Return current temperature in Fahrenheit degrees'''
-    temperature = owm.weather_at_place(place_name).get_weather().get_temperature('fahrenheit')['temp']
+    temperature = owm.weather_at_id(city_id).get_weather().get_temperature('fahrenheit')['temp']
     return temperature
 
 
 def owm_humidity(place_name):
     '''Return current humidity'''
-    humidity = owm.weather_at_place(place_name).get_weather().get_humidity()
+    humidity = owm.weather_at_id(city_id).get_weather().get_humidity()
     return humidity
 
